7b05badd4204a94ccb0d6a8601f6231a4be6875f
hyonestar19 <hanbyeol8378@hanyang.ac.kr>
2022-11-29 19:12:12 +0900

    KEY INPUT

--- Micro_Final_Proj.asm
+++ Micro_Final_Proj.asm
@@ -33,30 +33,30 @@ ST      EQU 17H ; SINGLE STEP KEY
 RST     EQU 18H ; RST KEY
 
 INDEX:  MOVC A, @A+PC ; 누산기는 1~24의 값을 가진다.
-KEYBASE:    DB  ST      ;SW1,ST 1
-            DB  INCR    ;SW6,INCREASE   2
-            DB  CD      ;SW11,CD    3
-            DB  REG     ;SW15, REG 4
-            DB  GO      ;SW19, GO   5
-            DB  0CH     ;SW2, C     6
-            DB  0DH     ;SW7, D     7
-            DB  0EH
-            DB  0FH
-            DB  COMMA
-            DB  O8H
-            DB  09H
-            DB  0AH
-            DB  0BH
-            DB  PERIOD
-            DB  04H
-            DB  05H
-            DB  06H
-            DB  07H
-            DB  RWKEY
-            DB  00H
-            DB  01H
-            DB  02H
-            DB  03H
-            DB  RST
+KEYBASE:    DB  ST      ;SW1,ST             1
+            DB  INCR    ;SW6,INCREASE       2
+            DB  CD      ;SW11,CD            3
+            DB  REG     ;SW15, REG          4
+            DB  GO      ;SW19, GO           5
+            DB  0CH     ;SW2, C             6
+            DB  0DH     ;SW7, D             7
+            DB  0EH     ;SW12, E            8
+            DB  0FH     ;SW16, F            9
+            DB  COMMA   ;SW20, COMMA(,)     10
+            DB  O8H     ;SW3, 8             11
+            DB  09H     ;SW8, 9             12
+            DB  0AH     ;SW13, A            13
+            DB  0BH     ;SW17, B            14
+            DB  PERIOD  ;SW21, PERIOD(.)    15
+            DB  04H     ;SW4, 4             16
+            DB  05H     ;SW9, 5             17
+            DB  06H     ;SW14, 6            18
+            DB  07H     ;SW18, 7            19
+            DB  RWKEY   ;SW22, R/W          20
+            DB  00H     ;SW5, 0 17          21   
+            DB  01H     ;SW10, 1            22
+            DB  02H     ;SW24, 2            23
+            DB  03H     ;SW23, 3            24
+            DB  RST     ;SW24, RST KEY      25
 
             END
